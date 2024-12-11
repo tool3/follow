751e7815b55f709aa0aac558b1e3844d62803f8d
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-11 14:40:38 +0200

    chore: paranoia

--- app/components/Credit.js
+++ app/components/Credit.js
@@ -5,7 +5,7 @@ const Credits = () => {
 
     return (
         <div className="credits">
-            Made with class by <a target="_blank" href="https://github.com/tool3">Tal Hayut</a>
+            Made with paranoia by <a target="_blank" href="https://github.com/tool3">Tal Hayut</a>
         </div>
     )
 };
